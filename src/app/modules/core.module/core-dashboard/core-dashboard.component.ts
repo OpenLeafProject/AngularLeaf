6e0f97c75f5c1af245bb28f29b6c13f292a14d23
Alex Latorre <alex.latorre@ginsa.cat>
2020-08-04 11:13:08 +0200

    dashboard modifications (not finished yet)

--- src/app/modules/core.module/core-dashboard/core-dashboard.component.ts
+++ src/app/modules/core.module/core-dashboard/core-dashboard.component.ts
@@ -1,24 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { PatientService } from '../../../services/data/patient.service';
+import { Patient } from '../../../models/api/patient.model';
 
 @Component({
   selector: 'app-core-dashboard',
   templateUrl: './core-dashboard.component.html',
   styleUrls: ['./core-dashboard.component.sass'],
-  providers: [MatSnackBar]
+  providers: [MatSnackBar, PatientService]
 })
 export class CoreDashboardComponent implements OnInit {
 
   constructor(
-    private _snackBar: MatSnackBar
+    private _snackBar: MatSnackBar,
+    private _patientService: PatientService
   ) {}
 
+  patient: Patient;
+
   ngOnInit(): void {
+
+  }
+
+  refresh() {
+    this._patientService.Load(1).subscribe(
+      result => {
+        this.patient = result;
+      }, error =>  {
+        // do nothing
+        
+      });
   }
 
   openSnackBar(message: string, action: string) {
     this._snackBar.open(message, action, {
       duration: 5000,
     });
+  }
+
+  stringify(value: any): string {
+    return JSON.stringify(value, undefined, 4);
   }
 }
